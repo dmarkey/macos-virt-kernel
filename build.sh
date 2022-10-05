f3b58c724436f0d9c778a683deb7dcab4b27e4d8
David Markey <dmarkey@Davids-MacBook-Pro.local>
2022-10-05 15:39:49 +0100

    Shoudl be good

--- build.sh
+++ build.sh
@@ -18,17 +18,15 @@ if [ ! -d linux ]; then
 fi
 cd linux
 cp ../linux.config .config
-#make
+make
 if [ ! "aarch64" = "$(uname -m)" ]; then
     echo "cross compiling on $(uname -m)"
     export CROSS_COMPILE=aarch64-pc-linux-gnu
 fi
-#ARCH=arm64 make CC=clang LLVM=1 LLVM_IAS=1 -j2 $*
+ARCH=arm64 make CC=clang LLVM=1 LLVM_IAS=1 -j2 $*
 cp .config ../linux.config
-#cp arch/arm64/boot/Image ../vmlinuz-arm64
-#cp arch/x86/boot/bzImage ../vmlinuz-amd64
-echo "foo" >> ../vmlinuz-arm64
-echo "bar" >> ../vmlinuz-amd64
+cp arch/arm64/boot/Image ../vmlinuz-arm64
+cp arch/x86/boot/bzImage ../vmlinuz-amd64
 
 cd ..
 gzip vmlinuz-arm64
