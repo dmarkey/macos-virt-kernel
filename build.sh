c5d014674b822a49a8c13efb598d408ea1fe05dd
David Markey <dmarkey@Davids-MacBook-Pro.local>
2022-10-05 14:43:29 +0100

    Try this

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
+wget https://github.com/cli/cli/releases/download/v2.17.0/gh_2.17.0_linux_amd64.deb
+dpkg -i gh_2.17.0_linux_amd64.deb
 if [ ! -d mnt ]; then
    apt update
-   apt install -y wget xz-utils patch bc make clang llvm lld flex bison libelf-dev libncurses-dev libssl-dev
+   apt install -y wget xz-utils patch bc make clang llvm lld flex bison libelf-dev libncurses-dev libssl-dev git 
 fi
 
 if [ ! -d linux ]; then
@@ -32,7 +33,7 @@ gzip vmlinuz-arm64
 mv vmlinuz-arm64.gz vmlinuz-arm64
 echo 
 echo Branch:$BRANCH
-if [ "$BRANCH" = "master" ] ; then
+if [[ "$BRANCH" == "master" ]] ; then
     mkdir release_assets
     cp vmlinuz-arm64 release_assets
     cp vmlinuz-amd64 release_assets
